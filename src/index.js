09dec67c3f5455504b10895fd4229c7389ed17a0
fdoquezada <fdoquezadapuno@gmail.com>
2024-03-29 17:07:26 -0300

    creacion de la platilla ejs y multer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const sharp = require('sharp');
-
+const path = require('path');
+// Initilizations
 const app = express();
 app.use(cors());
 
-const PORT = 3000
 
+//setings
+const PORT = 3000;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+//middlewares
+app.use(multer({
+    dest: path.join(__dirname, 'public/imagenes')
+}).single('image'));
+
+//Router
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+    res.send('uploaded');
+});
 
 
 
